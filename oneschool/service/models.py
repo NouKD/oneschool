4ea011e2bbb410047321f40ec39acadf2000fa91
NouKD <NouKD-n09916538@gmail.com>
2020-11-18 10:06:33 +0000

    update

--- oneschool/service/models.py
+++ oneschool/service/models.py
@@ -1,11 +1,102 @@
 from django.db import models
-from website.models import Etudiant, Commentaire, Teacher  
 
-# Create your models here.
+class Actualite(models.Model):
+
+    nom = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="images/Actualite")
+    description = models.TextField()
+    date = models.DateField()
+    comentaire = models.IntegerField()
+    vue = models.IntegerField()
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    
+    class Meta:
+
+        verbose_name = 'Actualite'
+        verbose_name_plural = 'Actualites'
+
+    def __str__(self):
+       return self.nom
+
+class Programme(models.Model):
+
+    titre = models.CharField(max_length=70)
+    image = models.ImageField(upload_to='image/Programme')
+    description = models.TextField()
+    nbr_diplome = models.IntegerField()
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    
+    class Meta:
+
+        verbose_name = 'Programme'
+        verbose_name_plural = 'Programmes'
+
+    def __str__(self):
+       return self.titre
+
+
+class Etudiant(models.Model):
+
+    nom = models.CharField(max_length=40)
+    cours_suivie = models.ManyToManyField(Cours, related_name='Etudiant')
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    
+    class Meta:
+
+        verbose_name = 'Etudiant'
+        verbose_name_plural = 'Etudiants'
+
+    def __str__(self):
+       return self.nom
+
+
+class Teacher(models.Model):
+    nom = models.CharField(max_length=40)
+    prenom = models.CharField(max_length=250)
+    image = models.ImageField()
+    cours_dispancer = models.ManyToManyField(Cours, related_name='Teacher')
+    
+    date_update = models.DateTimeField(auto_now_add=True)
+    date_add = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Teacher'
+        verbose_name_plural = 'Teachers'
+
+    def __str__(self):
+        return self.nom
+
+class Commentaire(models.Model):
+    nom = models.CharField(max_length=40)
+    prenom = models.CharField(max_length=250)
+    image = models.ImageField()
+    cours = models.ManyToManyField(Cours, related_name='Commentaire')
+    message = models.TextField()
+    
+    date_update = models.DateTimeField(auto_now_add=True)
+    date_add = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Commentaire'
+        verbose_name_plural = 'Commentaires'
+
+    def __str__(self):
+        return self.nom
 
 class Cours(models.Model):
+
     nom = models.CharField(max_length=50)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images/cours")
     prix = models.FloatField()
     duree = models.DurationField()
     nbr_chap = models.IntegerField()
@@ -13,13 +104,15 @@ class Cours(models.Model):
     etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE, related_name='Cours')
     commentaire = models.ForeignKey(Commentaire, on_delete=models.CASCADE, related_name='Cours')
     prof = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='Cours')
-    
-    date_update = models.DateTimeField(auto_now_add=True)
-    date_add = models.DateTimeField(auto_now=True)
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
     
     class Meta:
+
         verbose_name = 'Cours'
         verbose_name_plural = 'Cours'
-        def __str__(self):
-            return self.nom
+
+    def __str__(self):
+       return self.nom
--- oneschool/website/models.py
+++ oneschool/website/models.py
@@ -1,39 +1,43 @@
 from django.db import models
-from service.models import Cours
+from service.models import Actualite, 
 # Create your models here.
 
+
 class Programme(models.Model):
+
     titre = models.CharField(max_length=70)
-    image = models.ImageField(upload_to='image')
+    image = models.ImageField(upload_to='image/Programme')
     description = models.TextField()
     nbr_diplome = models.IntegerField()
-    
-    
-    date_update = models.DateTimeField(auto_now_add=True)
-    date_add = models.DateTimeField(auto_now=True)
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
     
     class Meta:
+
         verbose_name = 'Programme'
         verbose_name_plural = 'Programmes'
-        def __str__(self):
-            return self.nom
 
+    def __str__(self):
+       return self.titre
 
 
 class Etudiant(models.Model):
+
     nom = models.CharField(max_length=40)
     cours_suivie = models.ManyToManyField(Cours, related_name='Etudiant')
-    
-    date_update = models.DateTimeField(auto_now_add=True)
-    date_add = models.DateTimeField(auto_now=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
     
     class Meta:
+
         verbose_name = 'Etudiant'
         verbose_name_plural = 'Etudiants'
-        def __str__(self):
-            return self.nom
+
+    def __str__(self):
+       return self.nom
 
 
 class Teacher(models.Model):
@@ -49,14 +53,15 @@ class Teacher(models.Model):
     class Meta:
         verbose_name = 'Teacher'
         verbose_name_plural = 'Teachers'
-        def __str__(self):
-            return self.nom
+
+    def __str__(self):
+        return self.nom
 
 class Commentaire(models.Model):
     nom = models.CharField(max_length=40)
     prenom = models.CharField(max_length=250)
     image = models.ImageField()
-    cours = models.ManyToManyField(Cours, related_name='Teacher')
+    cours = models.ManyToManyField(Cours, related_name='Commentaire')
     message = models.TextField()
     
     date_update = models.DateTimeField(auto_now_add=True)
@@ -64,7 +69,8 @@ class Commentaire(models.Model):
     status = models.BooleanField(default=True)
     
     class Meta:
-        verbose_name = 'Teacher'
-        verbose_name_plural = 'Teachers'
-        def __str__(self):
-            return self.nom
+        verbose_name = 'Commentaire'
+        verbose_name_plural = 'Commentaires'
+
+    def __str__(self):
+        return self.nom
